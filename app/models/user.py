919f12cd8eb2b4f0396f5877b4cc0434857f8dcf
FranciscoFernandezz <fransmandes@gmail.com>
2025-09-16 17:13:39 -0300

    .

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-""" from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.orm import relationship
 from app.models.usuario_materia import usuario_materia
 from app.database.database import Base
@@ -19,40 +19,4 @@ class User(Base):
         "Materia",
         secondary=usuario_materia,
         back_populates="usuarios"
-    ) """
-
-
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-from app.database.database import get_db
-from app.models.user import User  # tu modelo SQLAlchemy
-from pydantic import BaseModel
-
-app = FastAPI()
-
-class UserCreate(BaseModel):
-    nombre: str
-    apellido: str
-    mail: str
-    contrasena: str
-    is_profe: bool
-    dni: str | None = None  # opcional porque en tu modelo es nullable
-
-@app.post("/usuarios/")
-def crear_usuario(usuario: UserCreate, db: Session = Depends(get_db)):
-    nuevo_usuario = User(
-        nombre=usuario.nombre,
-        apellido=usuario.apellido,
-        mail=usuario.mail,
-        contrasena=usuario.contrasena,
-        is_profe=usuario.is_profe,
-        dni=usuario.dni
     )
-    try:
-        db.add(nuevo_usuario)
-        db.commit()
-        db.refresh(nuevo_usuario)
-        return nuevo_usuario
-    except Exception as e:
-        db.rollback()
-        raise HTTPException(status_code=500, detail=str(e))
